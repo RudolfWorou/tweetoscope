9dc94befc47f9c50d042bb1fa0b7c72ddc5df185
Kimala-Kahotwa Divin <divin.kimala@student-cs.fr>
2021-11-14 13:33:19 +0100

    Correction de bugs

--- Collector/Processor.hpp
+++ Collector/Processor.hpp
@@ -37,14 +37,29 @@ private:
     priority_queue::handle_type queue_file_d_attente;
 
     //Collection2
-    //std::map<tweetoscope::timestamp, std::weak_ptr<ref>> fifo_collection2;
+    std::map<tweetoscope::timestamp, std::queue<std::weak_ptr<ref>>> fifo_collection2;
 
-    //Collection3
-    //std::map<tweetoscope::source::idf, std::weak_ptr<ref>> collection3;
+    /*
+
+    Collection3 
+
+    agit comme une table de symboles, de sorte que
+    l'obtention de l'instance de la cascade à partir de son identifiant est facile.
+    
+    */
+
+    std::map<std::string idef, std::weak_ptr<ref>> collection3;
 
 public:
-    std::vector<Cascade> add_tweet_in_cascade(std::string key, int T_obs, int T_obs2 int min_cascade_size, int termined, tweetoscope::tweet &t)
+    /*
+    
+    Ajoute une nouvelle cascade ou un nouveau tweet(retweet) dans un processus connaissant sa source
+    
+    */
+
+    void add_tweet_in_cascade(std::string key, int T_obs, int T_obs2 int min_cascade_size, int termined, tweetoscope::tweet &t)
     {
+
         auto source = t.source;
         auto type = t.type;
         auto msg = t.msg;
@@ -52,41 +67,45 @@ public:
         auto t = t.time;
         auto info = t.info;
 
-        if (queue_file_d_attente.size() == 0)
+        if (type == "tweet")
         {
             std::vector<std::array<double, 2>> tweets;
             tweets.push_back({t, m});
             auto ref1 = make_cascade(type, key, msg, T_obs, tweets);
             queue_file_d_attente.push(ref1);
 
-            /*fifo_collection2[T_obs] = ref1;
-            fifo_collection2[T_obs2] = ref1;
-            collection3[source] = ref1;*/
+            fifo_collection2[T_obs].enqueue(ref1);
+            fifo_collection2[T_obs2].enqueue(ref1);
+            collection3[key] = ref1;
         }
         else
         {
-            for (auto &c : queue_file_d_attente)
+            //C'est un retweet
+            bool cascade_exist = !(collection3.find(key) == collection3.end());
+
+            if (cascade_exist)
             {
-                if (c.cid == key)
-                {
-                    c.tweets.push_back({t, m});
-                    /*fifo_collection2[T_obs] = c;
-                    fifo_collection2[T_obs2] = c;
-                    collection3[source] = c;*/
-                    break;
-                }
+                auto cascade = collection3[key];
+                cascade.tweets.push_back({t, m});
+                fifo_collection2[T_obs].enqueue(cascade);
+                fifo_collection2[T_obs2].enqueue(cascade);
             }
         }
-
-        return cascades_termine(termined, min_cascade_size, t);
     }
+
+    /*
+    
+    Retourne les cascades partielles à envoyer.
+    
+    */
 
     std::vector<Cascade> cascade_partielles(int T_obs)
     {
         std::vector<Cascade> resultat;
-        for (auto &c : fifo_collection2)
+        for (auto &c : fifo_collection2[T_obs])
         {
-            if (min_cascade_size <= c.tweets.size() && c.(tweets.end() - 1).first() >= T_obs)
+            auto v = c.(tweets.end() - 1).first() - c.(tweets.begin() - 1).first();
+            if (min_cascade_size <= c.tweets.size() && v >= T_obs)
             {
                 Cascade d{c.type, c.cid, c.msg, c.T_obs, c.tweets};
                 resultat.push_back(d);
@@ -95,12 +114,17 @@ public:
         return resultat;
     }
 
-    std::vector<Cascade> cascades_termine(int termined, int min_cascade_size, int temps)
+    /*
+    
+    Retourne les cascades finales à envoyer.
+    
+    */
+    std::vector<Cascade> cascades_termine(int termined, int min_cascade_size)
     {
         std::vector<Cascade> resultat;
         for (auto &c : queue_file_d_attente)
         {
-            if (min_cascade_size <= c.tweets.size() && (temps - c.(tweets.end() - 1).first()) >= termined)
+            if (min_cascade_size <= c.tweets.size() && (c.(tweets.end() - 1).first() - termined) >= termined)
             {
                 Cascade d{c.type, c.cid, c.msg, c.T_obs, c.tweets};
                 resultat.push_back(d);
@@ -110,6 +134,16 @@ public:
         return resultat;
     }
 };
+
+/*
+Les fonctions qui n'appartiennent pas à la classe Processor
+
+                            ||
+                            ||
+                            ||
+                            \/
+
+*/
 
 bool element_ref_comparator::operator()(ref op1, ref op2) const
 {
